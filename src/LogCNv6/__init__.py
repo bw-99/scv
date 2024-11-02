cc59d01c09073e3f6f4b58293b7d8943585c7f9f
bw-99 <bb1702@g.skku.com>
2024-11-02 17:51:59 +0000

    add: logcnv6_v5, logcnv6_v6

--- src/LogCNv6/__init__.py
+++ src/LogCNv6/__init__.py
@@ -2,3 +2,5 @@ from .LogCNv6_v1 import LogCNv6_v1
 from .LogCNv6_v2 import LogCNv6_v2
 from .LogCNv6_v3 import LogCNv6_v3
 from .LogCNv6_v4 import LogCNv6_v4
+from .LogCNv6_v5 import LogCNv6_v5
+from .LogCNv6_v6 import LogCNv6_v6

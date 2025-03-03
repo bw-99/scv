2ed92bbe9dba2fa9c2e0ea270b24a5f58a83a2a3
dwen-99 <bb1702@g.skku.edu>
2025-03-03 21:01:48 +0900

    add: scv heavy loca

--- src/SCV/__init__.py
+++ src/SCV/__init__.py
@@ -1,5 +1,6 @@
 from .SCV import SCV
 from .SCV_ada_tf import SCV_ada_tf
 from .SCV_light_loca import SCV_light_loca
-from .SCV_light import SCV_light
 from .SCV_loca import SCV_loca
+from .SCV_light import SCV_light
+from .SCV_heavy_loca import SCV_heavy_loca
